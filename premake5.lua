9492d12b03642fadd59dbdd8091c731f2e393425
Callum Pertoldi <callumpjpertoldi@gmail.com>
2020-12-07 08:39:41 +0000

    - Using premake to build project files.

---
+++ premake5.lua
@@ -0,0 +1,50 @@
+workspace "Tests"
+	architecture "x64"
+	startproject "VectorTest"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	platforms { "Win32", "Win64" }
+
+	filter { "platforms:Win32" }
+		system "Windows"
+		architecture "x86"
+
+	filter { "platforms:Win64" }
+		system "Windows"
+		architecture "x86_64"
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Project list here
+
+project "VectorTest"
+	kind "ConsoleApp"
+	language "C++"
+	location "VectorTest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/" .. outputdir .. "/obj/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp" 
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines {"DEBUG"}
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines {"NDEBUG"}
+		runtime "Release"
+		optimize "on"
